394a6b6f82425f1886ce427573dbb4a470f2dc55
xusifob <b.malahieude@free.fr>
2015-09-29 15:32:17 +0200

    add doc

--- templates/doc/kinds-of-fields.php
+++ templates/doc/kinds-of-fields.php
@@ -1,3 +1,612 @@
 <div role="tabpanel" class="tab-pane active fade in" id="kinds-of-fields">
+    <h2>text</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>placeholder</td>
+            <td>Le texte affiché dans le champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>valeur</td>
+            <td>La valeur par défault du champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Autocomplete</td>
+            <td>Si le champ peut être remplis dynamiquement par des propositions</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>email</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>placeholder</td>
+            <td>Le texte affiché dans le champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>valeur</td>
+            <td>La valeur par défault du champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Autocomplete</td>
+            <td>Si le champ peut être remplis dynamiquement par des propositions</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>password</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>placeholder</td>
+            <td>Le texte affiché dans le champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>valeur</td>
+            <td>La valeur par défault du champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Autocomplete</td>
+            <td>Si le champ peut être remplis dynamiquement par des propositions</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>repeatPassword</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>placeholder</td>
+            <td>Le texte affiché dans le champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>valeur</td>
+            <td>La valeur par défault du champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Autocomplete</td>
+            <td>Si le champ peut être remplis dynamiquement par des propositions</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>number</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>placeholder</td>
+            <td>Le texte affiché dans le champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>valeur</td>
+            <td>La valeur par défault du champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Autocomplete</td>
+            <td>Si le champ peut être remplis dynamiquement par des propositions</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>tel</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>placeholder</td>
+            <td>Le texte affiché dans le champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>valeur</td>
+            <td>La valeur par défault du champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Autocomplete</td>
+            <td>Si le champ peut être remplis dynamiquement par des propositions</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>date</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>placeholder</td>
+            <td>Le texte affiché dans le champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>valeur</td>
+            <td>La valeur par défault du champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Autocomplete</td>
+            <td>Si le champ peut être remplis dynamiquement par des propositions</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>checkbox</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>valeur</td>
+            <td>La valeur par défault du champ</td>
+            <td>(vide)</td>
+            <td>(vide)</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>select</h2>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom de l'option</th>
+            <th>Description</th>
+            <th>Valeur par défaut</th>
+            <th>valeur si vide</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>id</td>
+            <td>L'id du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ</td>
+        </tr>
+        <tr>
+            <td>class</td>
+            <td>La class du champ</td>
+            <td>(vide)</td>
+            <td>class par défault</td>
+        </tr>
+        <tr>
+            <td>label</td>
+            <td>Le label du champ</td>
+            <td>(vide)</td>
+            <td>nom du champ (avec class sr-only)</td>
+        </tr>
+        <tr>
+            <td>class du label</td>
+            <td>La class du label du champ</td>
+            <td>(vide)</td>
+            <td>(vide ou sr-only si le label est vide)</td>
+        </tr>
+        <tr>
+            <td>required</td>
+            <td>Si le champ est requis ou pas</td>
+            <td>coché</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Placer le label après le champ</td>
+            <td>Mettre le label sous le champ dans le code</td>
+            <td>décoché (au dessus)</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Trier les champs par</td>
+            <td>Ordre des options affichés dans le select</td>
+            <td>Default (l'ordre des options entrés en dessous)</td>
+            <td>x</td>
+        </tr>
+        </tbody>
+    </table>
 
+    <p>Champ option</p>
+
+    <table>
+        <thead>
+        <tr>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>
+
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+    <h2>radio</h2>
+    <h2>url</h2>
+    <h2>range</h2>
+    <h2>color</h2>
+    <h2>search</h2>
+    <h2>hidden</h2>
+    <h2>file</h2>
+    <h2>textarea</h2>
+    <h2>taxonomy</h2>
+    <h2>wp_editor</h2>
+    <h2>open container</h2>
+    <h2>close container</h2>
+    <h2>close all container</h2>
 </div>
