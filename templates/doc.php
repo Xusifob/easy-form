8540dfbcacd1ab5bdfd1bbfb41b04d65a045af12
xusifob <b.malahieude@free.fr>
2015-10-16 17:15:21 +0200

    add hook doc

--- templates/doc.php
+++ templates/doc.php
@@ -6,10 +6,12 @@
             <ul id="myTabs" class="nav nav-tabs" role="tablist">
                 <li role="presentation" class="active"><a href="#kinds-of-fields" data-toggle="tab">Types de champs</a></li>
                 <li><a href="#kinds-of-forms" data-toggle="tab">Types de formulaires</a></li>
+                <li><a href="#hooks" data-toggle="tab">Hooks</a></li>
             </ul>
             <div class="tab-content">
                 <?php include __DIR__ . '/doc/kinds-of-fields.php'; ?>
                 <?php include __DIR__ . '/doc/kinds-of-forms.php'; ?>
+                <?php include __DIR__ . '/doc/hooks.php'; ?>
             </div>
         </div>
 
---
+++ templates/doc/hooks.php
@@ -0,0 +1,5 @@
+<div role="tabpanel" class="tab-pane fade " id="hooks">
+    <h2>Ajouter un post</h2>
+
+    <p>Il existe 2 posts </p>
+</div>
