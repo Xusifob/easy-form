9140a24ee4de84607aaf1223a5e1a184a07be8ac
xusifob <b.malahieude@free.fr>
2015-10-31 16:37:22 +0100

    add doc for hooks

--- templates/doc.php
+++ templates/doc.php
@@ -14,54 +14,5 @@
                 <?php include __DIR__ . '/doc/hooks.php'; ?>
             </div>
         </div>
-
-
-        <!--
-                <h2>Hooks & actions</h2>
-                <p>Il est possible d'effectuer des actions lors de la validation des formulaires, pour celà, il suffit d'utiliser les hooks.
-                Pour chaque type de hook, il en existe 2, un "global" qui va être lié à tous les formulaires faisant la même action (tous les formulaires d'inscription par exemple
-                Le second type est "lié au formulaire" pour ça, il suffit d'ajouter l'ID du formulaire </p>
-                <pre class="brush: php">
-                /** Permet d'effectuer une action à l'ajout d'un post  **/
-                    add_action('form/insertPost','myInsertPost');
-                    add_action('form/insertPost-$formId','myInsertPost');
-                    function myInsertPost(){
-                    // Action
-                    }
-
-                /** Permet d'effectuer une action à l'ajout ou la modification d'un post **/
-                    add_action('form/insertOrModifyPost','myInsertPost');
-                    function myInsertPost(){
-                    // Action
-                    }
-
-                /** Permet d'effectuer une action à la modification d'un post **/
-                    add_action('form/ModifyPost','myInsertPost');
-                    function myInsertPost(){
-                    // Action
-                    }
-
-                /** Permet d'effectuer une action à l'ajout ou la modification d'un utilisateur **/
-                    add_action('form/insertOrModifyUser','myInsertPost');
-                    function myInsertPost(){
-                    // Action
-                    }
-
-                /** Permet d'effectuer une action à l'ajout d'un utilisateur **/
-                    add_action('form/ModifyUser','insertUser');
-                    function myInsertPost(){
-                    // Action
-                    }
-
-                /** Permet d'effectuer une action à la modification d'un utilisateur **/
-                    add_action('form/ModifyUser','myInsertPost');
-                    add_action('form/ModifyUser','myInsertPost');
-                    function myInsertPost(){
-                    // Action
-                    }
-                </pre>-->
-
-
-
     </section>
 </div>
--- templates/doc/hooks.php
+++ templates/doc/hooks.php
@@ -3,12 +3,180 @@
 
     <p>Les Hooks (ou ancres) permettent d'effectuer des actions à un certain moment de l'execution d'un script. Il en existe plusieurs sur ces formulaires qui permet d'agir lors de leur fonctionnement (pour traiter des données bien spécifiques ou effectuer une action après l'envoi du formulaire par exemple...)</p>
 
-    <h3>Ajouter un post</h3>
+    <h3>Fonctionnement des hooks</h3>
 
-    <pre class="brush brush:php">
+    <p>Les fonctions prennent parfois des arguments, parfois non. tout ceci sera expliqué dans la documentation. Les arguments sont des données que l'on peut récupérer et traiter dans la fonction. Il est inutile qu'elle ne retournent quoi que ce soit, il ne sera pas utilisé.</p>
+
+    <pre class="brush: php">
         &lt;?php
-        add_action();
+        add_action('myhook','myfunction');
 
+        /**
+         * @param $args tous les arguments de la fonction
+         */
+        function myfunction($args){
+        // Action que l'on souhaite effectuer
+        }
         ?>
     </pre>
+
+    <p>Tous les hooks doivent être placés avant l'appel aux formulaires, sinon ils ne seront pas appelés</p>
+
+    <pre class="brush: php">
+        &lt;?php
+        add_action('myhook','myfunction');
+
+        function myfunction($postId){
+            // Action que l'on souhaite effectuer
+        }
+
+        $form = new WP_Form(56);
+
+        echo $form;
+        ?>
+    </pre>
+
+
+    <h3>Ajouter un post</h3>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom du hook</th>
+            <th>Arguments</th>
+            <th>Description</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>form/BeforeInsertOrModifyPost</td>
+            <td>$postId (id du post à modifier)</td>
+            <td>Agis <strong>avant</strong> l'insertion ou la modification d'un post, quelque soit le formulaire utilisé</td>
+        </tr>
+        <tr>
+            <td>form/BeforeInsertOrModifyPost-{{formId}}</td>
+            <td>$postId (id du post à modifier)</td>
+            <td>Agis <strong>avant</strong> l'insertion ou la modification d'un post. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        <tr>
+            <td>form/BeforeModifyPost</td>
+            <td>$postId (id du post à modifier)</td>
+            <td>Agis uniquement <strong>avant</strong> la modification d'un post, quelque soit le formulaire utilisé</td>
+        </tr>
+        <tr>
+            <td>form/BeforeInsertOrModifyPost-{{formId}}</td>
+            <td>$postId (id du post à modifier)</td>
+            <td>Agis uniquement <strong>avant</strong> la modification d'un post. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        <tr>
+            <td>form/InsertOrModifyPost</td>
+            <td>$postId (id du post à modifier)</td>
+            <td>Agis <strong>après</strong> l'insertion ou la modification d'un post, quelque soit le formulaire utilisé</td>
+        </tr>
+        <tr>
+            <td>form/InsertOrModifyPost-{{formId}}</td>
+            <td>$postId (id du post à modifier)</td>
+            <td>Agis <strong>après</strong> l'insertion ou la modification d'un post. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        <tr>
+            <td>form/ModifyPost</td>
+            <td>$postId (id du post à modifier)</td>
+            <td>Agis uniquement <strong>après</strong> la modification d'un post, quelque soit le formulaire utilisé</td>
+        </tr>
+        <tr>
+            <td>form/InsertOrModifyPost-{{formId}}</td>
+            <td>$postId (id du post à modifier)</td>
+            <td>Agis uniquement <strong>après</strong> la modification d'un post. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <h3>Ajouter un post</h3>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom du hook</th>
+            <th>Arguments</th>
+            <th>Description</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>form/BeforeInsertOrModifyUser</td>
+            <td>$userId (id de l'utilisateur à modifier)</td>
+            <td>Agis <strong>avant</strong> l'insertion ou la modification d'un utilisateur, quelque soit le formulaire utilisé</td>
+        </tr>
+        <tr>
+            <td>form/BeforeInsertOrModifyUser-{{formId}}</td>
+            <td>$userId (id de l'utilisateur à modifier)</td>
+            <td>Agis <strong>avant</strong> l'insertion ou la modification d'un utilisateur. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        <tr>
+            <td>form/BeforeModifyUser</td>
+            <td>$userId (id de l'utilisateur à modifier)</td>
+            <td>Agis uniquement <strong>avant</strong> la modification d'un utilisateur, quelque soit le formulaire utilisé</td>
+        </tr>
+        <tr>
+            <td>form/BeforeInsertOrModifyUser-{{formId}}</td>
+            <td>$userId (id de l'utilisateur à modifier)</td>
+            <td>Agis uniquement <strong>avant</strong> la modification d'un utilisateur. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        <tr>
+            <td>form/InsertOrModifyUser</td>
+            <td>$userId (id de l'utilisateur à modifier)</td>
+            <td>Agis <strong>après</strong> l'insertion ou la modification d'un utilisateur, quelque soit le formulaire utilisé</td>
+        </tr>
+        <tr>
+            <td>form/InsertOrModifyUser-{{formId}}</td>
+            <td>$userId (id de l'utilisateur à modifier)</td>
+            <td>Agis <strong>après</strong> l'insertion ou la modification d'un utilisateur. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        <tr>
+            <td>form/ModifyUser</td>
+            <td>$userId (id de l'utilisateur à modifier)</td>
+            <td>Agis uniquement <strong>après</strong> la modification d'un utilisateur, quelque soit le formulaire utilisé</td>
+        </tr>
+        <tr>
+            <td>form/InsertOrModifyUser-{{formId}}</td>
+            <td>$userId (id de l'utilisateur à modifier)</td>
+            <td>Agis uniquement <strong>après</strong> la modification d'un utilisateur. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        </tbody>
+    </table>
+
+
+
+    <h3>Envoi de mail </h3>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Nom du hook</th>
+            <th>Arguments</th>
+            <th>Description</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>form/BeforeSendMail</td>
+            <td>aucun</td>
+            <td>Agis <strong>avant</strong> d'envoyer un e-mail de contact quelque soit le formulaire utilisé.</td>
+        </tr>
+        <tr>
+            <td>form/BeforeSendMail-{{formId}}</td>
+            <td>aucun</td>
+            <td>Agis <strong>avant</strong> d'envoyer un e-mail de contact. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>form/SendMail</td>
+            <td>aucun</td>
+            <td>Agis <strong>après</strong> avoir envoyé un e-mail de contact quelque soit le formulaire utilisé.</td>
+        </tr>
+        <tr>
+            <td>form/SendMail-{{formId}}</td>
+            <td>aucun</td>
+            <td>Agis <strong>après</strong> avoir envoyé un e-mail de contact. Remplacer {{formId}} par l'id d'un formulaire permet de cibler celui là spécifiquement.</td>
+        </tr>
+        </tbody>
+    </table>
+
 </div>
