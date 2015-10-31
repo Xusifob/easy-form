9bdcae05594902b42a4a2c48efdb0112f6565f4a
xusifob <b.malahieude@free.fr>
2015-10-31 16:04:20 +0100

    start hooks

--- templates/doc/hooks.php
+++ templates/doc/hooks.php
@@ -1,5 +1,14 @@
 <div role="tabpanel" class="tab-pane fade " id="hooks">
-    <h2>Ajouter un post</h2>
+    <h2>Les Hooks</h2>
 
-    <p>Il existe 2 posts </p>
+    <p>Les Hooks (ou ancres) permettent d'effectuer des actions à un certain moment de l'execution d'un script. Il en existe plusieurs sur ces formulaires qui permet d'agir lors de leur fonctionnement (pour traiter des données bien spécifiques ou effectuer une action après l'envoi du formulaire par exemple...)</p>
+
+    <h3>Ajouter un post</h3>
+
+    <pre class="brush brush:php">
+        &lt;?php
+        add_action();
+
+        ?>
+    </pre>
 </div>
