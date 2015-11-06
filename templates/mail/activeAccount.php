7a249247f3b5d1e9450c66e24f7dfda2a9c9eb54
xusifob <b.malahieude@free.fr>
2015-11-06 12:27:46 +0100

    start add new user

---
+++ templates/mail/activeAccount.php
@@ -0,0 +1,8 @@
+Bonjour %NOM%,
+<br>
+<br>
+Bienvenue sur %BLOGNAME% ! Pour commencer, vous devez confirmer votre adresse e-mail.
+%LIEN%confirmer mon adresse e-mail%/LIEN%
+<br>
+Merci,
+L'équipe %BLOGNAME%
